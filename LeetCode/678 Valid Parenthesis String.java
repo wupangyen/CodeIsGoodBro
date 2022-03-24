e30a4f7a4fb5265e53a8ea100ef4be209c126ef3
wupangyen <72144016+wupangyen@users.noreply.github.com>
2022-03-23 22:58:06 -0400

    Update 678 Valid Parenthesis String.java

--- LeetCode/678 Valid Parenthesis String.java
+++ LeetCode/678 Valid Parenthesis String.java
@@ -19,3 +19,37 @@ class Solution:
                 return False
         
         return cmin == 0
+
+  # Solution with Stack                  
+  class Solution:
+    def checkValidString(self, s: str) -> bool:
+        
+        stack1 = []
+        stack2 = []
+        
+        for index,elem in enumerate(s):
+            
+            if elem == "(":
+                stack1.append(index)
+            elif elem == "*":
+                stack2.append(index)
+            
+            else:
+                # check if stack1 not empty
+                if stack1:
+                    stack1.pop()
+                elif stack2:
+                    stack2.pop()
+                else:
+                    return False
+        
+        
+        while stack1 and stack2:
+            pos1 = stack1.pop()
+            pos2 = stack2.pop()
+            
+            if pos2 < pos1:
+                return False
+        
+        return len(stack1) == 0
+            
